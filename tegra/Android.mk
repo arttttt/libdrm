b205d2d2af292cf2e85e3d6f67d57e654cbb39f3
arttttt <artem-bambalov@yandex.ru>
2019-01-26 14:57:19 +0300

    tegra: fix android makefile
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- tegra/Android.mk
+++ tegra/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-# Import variables LIBDRM_NOUVEAU_FILES, LIBDRM_NOUVEAU_H_FILES
+# Import variables LIBDRM_TEGRA_FILES, LIBDRM_TEGRA_H_FILES
 include $(LOCAL_PATH)/Makefile.sources
 
 LOCAL_MODULE := libdrm_tegra
 
 LOCAL_SHARED_LIBRARIES := libdrm
 
-LOCAL_SRC_FILES := $(LIBDRM_NOUVEAU_FILES)
+LOCAL_SRC_FILES := $(LIBDRM_TEGRA_FILES)
 
 include $(LIBDRM_COMMON_MK)
 include $(BUILD_SHARED_LIBRARY)
